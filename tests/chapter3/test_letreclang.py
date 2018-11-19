c7682481e73b111a6569b0822a643a69a561d5b2
Sriram Panyam <sri.panyam@gmail.com>
2018-11-18 23:30:44 -0800

    Using parser for eval tests

--- tests/chapter3/test_letreclang.py
+++ tests/chapter3/test_letreclang.py
@@ -3,19 +3,19 @@
 from ipdb import set_trace
 from epl.chapter3 import letreclang
 from epl.common import DefaultEnv as Env
-import parser
+from tests.chapter3.parserutils import parse
 
 Eval = letreclang.Eval
 
-def _test_double():
-    expr = parse("""
+def test_double():
+    expr,tree = parse("""
             letrec double(x) = if isz(x) then 0 else - ((double -(x,1)), -2)
             in (double 6)
             """)
     assert Eval().valueOf(expr, Env()) == 12
 
-def _test_oddeven():
-    expr = parse ("""
+def test_oddeven():
+    expr,tree = parse ("""
             letrec
                 even(x) = if isz(x) then 1 else (odd -(x,1))
                 odd(x) = if isz(x) then 0 else (even -(x,1))
